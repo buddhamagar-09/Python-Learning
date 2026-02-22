aa3cd261214a1e0886cacabfbd30be95f6452954
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-02-22 23:47:38 +0545

    Using Conditional ststements

---
+++ Basics/ConStatements.py
@@ -0,0 +1,23 @@
+# using if elif else statements
+
+# age = int(input("Enter your age: "))
+
+# if age <= 13:
+#     print("You are still a child")
+# elif age > 13 and age <= 19:
+#     print("You are a Teeneger")
+# else:
+#     print("You are adult bro:")
+
+
+    # example no 2
+
+response = input("Are you 18+ (Y/N): ")
+if response == "":
+    print("You didnt enter anything.")
+elif response == "Y":
+    print("Click this link to you videos. ")
+else:
+    print("You are Still a child for this")
+
+
