cc754df3ea1267da0bcbc13a154869ec8fccda1a
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-02-22 23:34:41 +0545

    area of the circle Calculated using math methods

---
+++ PracticeQuestions/que2.py
@@ -0,0 +1,16 @@
+import math as m
+#  calculate the area of the circle
+
+
+# radius = float(input("Enter the radius of the circle"))
+# pi = m.pi
+# area = pi*pow(radius,2)
+# print(f"The area of the circle is {area} cm^2.")
+
+
+# find side d of the triangle
+a = float(input("Enter the length of side a: "))
+b = float(input("Enter the length of the side b: "))
+
+c = m.sqrt(pow(a,2) + pow(b,2))
+print(f"The length of the side c is {c}")
