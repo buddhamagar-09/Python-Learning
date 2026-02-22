c26cff3de3112c6bff4de65e5a7867233882be5c
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-02-22 23:31:10 +0545

    Operators learned and some predefined math methods

---
+++ Basics/operatorsDemo.py
@@ -0,0 +1,47 @@
+import math as m
+
+
+
+apple = 5
+
+
+# apple = apple + 1
+# apple += 1
+
+# apple = apple -1
+# apple -= 1
+
+# apple = apple *3
+# apple *= 3
+
+# apple = apple / 3
+# apple /= 3
+
+# apple = apple ** 3
+# apple **= 3
+
+# print(apple)
+
+x = -3.145
+y = 4
+z = 5
+
+# result = round(x)
+# result = max(x,y,z)
+result = abs(x)
+# result = min(x,y,z)
+# result = pow(3,3)
+# print(result)
+
+# using math library
+a = 81.3
+
+# output = m.sqrt(a)
+output = m.ceil(a)
+# output = m.floor(a)
+print(output)
+
+
+
+
+
