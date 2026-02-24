f227785936d2ba6a23bf382955ec6a2981daf83b
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-02-24 21:00:36 +0545

    Looping in python for loop and while loop learned

---
+++ Basics/looping.py
@@ -0,0 +1,51 @@
+import time
+# while loop 
+# calculate interest
+
+# principal = 0
+# rate = 0
+# time = 0
+
+# while True:
+#     principal = int(input("Enter the Principal amount: "))
+#     if principal < 0:
+#         print("Principal amount cannot be less than zero.")
+#     else:
+#         break
+
+# while True:
+#     rate = float(input("Enter the rate of the interest: "))
+#     if rate < 0:
+#         print("Rate cannot be negative.")
+#     else:
+#         break
+
+# while True:
+#     time = float(input("Enter the time of the Interest: "))
+#     if time < 0:
+#         print("Time cannot be negative.")
+#     else:
+#         break
+
+# interest = (principal * time * rate)/100
+# print(f"The interest after {time} years is Rs.{interest:.2f}")
+
+
+
+# for loop for fixed number of time    
+
+
+# eg iterate for numbers
+
+# for x in reversed(range(1,10)):
+#     if x == 6:
+#         continue
+#     else:
+#         print(x)
+
+# iterate in strings
+name = input("Enter your Nmae: ")
+for x in name:
+    print(x)
+
+
