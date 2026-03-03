788b5ae40113c406bb63671b2671b73bfdb8e201
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-03-03 21:35:11 +0545

    Rps game upadted with main function

--- PracticeQuestions/RPSGame.py
+++ PracticeQuestions/RPSGame.py
@@ -1,49 +1,54 @@
 import random as rd
 
-options = ("rock","paper","scissors")
-play_game = True
 
-while play_game:
-    userscore = 0
-    computerscore = 0
-    is_running = True
+def main():
+    options = ("rock","paper","scissors")
+    play_game = True
 
-    print("----------------Rock Paper Scissors Game-----------------")
-    while is_running:
-        user_choice = input("Enter your Choice:  or type q to quit the game: ").lower()
-        if user_choice == "q":
-            is_running = False
-            continue
-        
-        computer_choice = rd.choice(options)
-        print(f"User Choice: {user_choice}")
-        print(f"Computer Choice: {computer_choice}")
-        
-        if user_choice not in options:
-            print("Invalid input! Please enter a valid choice: Rock, Paper, or Scissor.")
-        elif user_choice == computer_choice:
-            print("Its a Draw: ")
-        elif user_choice =="rock" and computer_choice == "scissors"  or user_choice == "paper" and computer_choice == "rock"   or user_choice == "scissors" and computer_choice == "paper":
-            userscore += 1
-            print(f"User Win !")
+    while play_game:
+        userscore = 0
+        computerscore = 0
+        is_running = True
+
+        print("----------------Rock Paper Scissors Game-----------------")
+        while is_running:
+            user_choice = input("Enter your Choice:  or type q to quit the game: ").lower()
+            if user_choice == "q":
+                is_running = False
+                continue
+            
+            computer_choice = rd.choice(options)
+            print(f"User Choice: {user_choice}")
+            print(f"Computer Choice: {computer_choice}")
+            
+            if user_choice not in options:
+                print("Invalid input! Please enter a valid choice: Rock, Paper, or Scissor.")
+            elif user_choice == computer_choice:
+                print("Its a Draw: ")
+            elif user_choice =="rock" and computer_choice == "scissors"  or user_choice == "paper" and computer_choice == "rock"   or user_choice == "scissors" and computer_choice == "paper":
+                userscore += 1
+                print(f"User Win !")
+            else:
+                computerscore += 1
+                print("Computer Won")
+
+        if userscore == computerscore:
+            print("Its a Draw!! Try again")
+        elif userscore > computerscore:
+            print("User Won this Competition!!")        
         else:
-            computerscore += 1
-            print("Computer Won")
+            print("Computer Won this Competition!! Try again")
+        print(f"userscore: {userscore} || computerscore: {computerscore}")
 
-    if userscore == computerscore:
-        print("Its a Draw!! Try again")
-    elif userscore > computerscore:
-        print("User Won this Competition!!")        
+        playAgain = input("\nWould you Like to Play Again (y/N): ").lower()
+        if playAgain != "y":
+            play_game = False
+            print("Thanks for playing! Goodbye!")
+        else:
+            print("\n")
     else:
-        print("Computer Won this Competition!! Try again")
-    print(f"userscore: {userscore} || computerscore: {computerscore}")
+        is_running = False
+        print("Thank Your For Playing The game. See you Soon!")
 
-    playAgain = input("\nWould you Like to Play Again (y/N): ").lower()
-    if playAgain != "y":
-        play_game = False
-        print("Thanks for playing! Goodbye!")
-    else:
-        print("\n")
-else:
-    is_running = False
-    print("Thank Your For Playing The game. See you Soon!")
+if __name__ == '__main__':
+    main()
