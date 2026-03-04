3dbf49ed6b66eda7977d1bfe11ec06ec1c662b14
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-03-04 19:02:06 +0545

    simple counter and countdown timer created

--- PracticeQuestions/que4.py
+++ PracticeQuestions/que4.py
@@ -4,21 +4,22 @@ import time
 
 my_time = int(input("Enter the time in seconds: "))
 
-for x in range(my_time,0,-1):
+for x in range(my_time,-1,-1):
     seconds = int (x % 60)
-    minutes = int(x/60) % 60
+    minutes = int(x//60) % 60
     hours = int(x / 3600)
     time.sleep(1)
-    print(f"{hours:02}:{minutes:02}:{seconds:02}")
-
+    print(f"\r{hours:02d}:{minutes:02d}:{seconds:02d}",end=" ",flush=True)
+print()
 print("Times up Buddy.")
 
 
 
 
-# sec = int(input("Enter the countdown Number you like: "))
+sec = int(input("Enter the countdown Number you like: "))
 
-# for counter in range(sec,0,-1):
-#     time.sleep(1)
-#     print(counter)
-# print("Happy New Year!!")
+for counter in range(sec,0,-1):
+    time.sleep(1)
+    print(f"{counter}")
+print()
+print("Happy New Year!!")
