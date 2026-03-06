ca9b43c76161438e2f0a6bf7b0d5fc1cafe6a02e
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-03-06 22:17:24 +0545

    Super key used to invoke the constructor of the parent class

---
+++ OOP/Super_Key_Demo.py
@@ -0,0 +1,34 @@
+
+
+
+class shapes:
+    def __init__(self, color, is_filled):
+        self.color = color
+        self.is_filled = is_filled
+
+class Rectagnle(shapes):
+    def __init__(self,name,color,is_filled,length,breadth):
+        super().__init__(color,is_filled)
+        self.name = name
+        self.length = length
+        self.breadth = breadth
+    def display_details(self):
+        print(f" Shape: {self.name} \n Length: {self.length} \n Breadth: {self.breadth}")
+        print(f"The {self.name} of length {self.length} and breadth {self.breadth} is {'filled' if self.is_filled else 'not filled'} with {self.color} color.")
+
+
+class Square(shapes):
+    def __init__(self,name,color,is_filled,length):
+        super().__init__(color,is_filled)
+        self.name = name
+        self.length = length
+    def display_details(self):
+        print(f" Shape: {self.name} \n Length: {self.length}")
+        print(f"The {self.name} of length {self.length}  is {'filled' if self.is_filled else 'not filled'} with {self.color} color.")
+
+rect = Rectagnle("Rectangle","Red",True,12,23)
+sq = Square("Square","Green",False,12)
+
+rect.display_details()
+print()
+sq.display_details()
